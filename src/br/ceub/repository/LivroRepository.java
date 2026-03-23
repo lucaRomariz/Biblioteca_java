0e82e6396b4fa9763352132be467354745e9cfc6
Luca Romariz <luca.romariz@icloud.com>
2026-03-23 09:16:07 -0300

    Add files via upload

---
+++ src/br/ceub/repository/LivroRepository.java
@@ -0,0 +1,29 @@
+package br.ceub.repository;
+import java.util.List;
+
+import br.ceub.model.Livro;
+
+public class LivroRepository {
+
+    public Livro salvar(Livro livro) {
+        return null;
+    }
+
+    public Livro buscarPorId(int id) {
+        return null;
+    }
+
+    public List<Livro> buscarPorNome(String nome) {
+        return null;
+    }
+
+    public List<Livro> listarTodos() {
+        return null;
+    }
+
+    public void atualizar(Livro livro) {
+    }
+
+    public void deletar(int id) {
+    }
+}
